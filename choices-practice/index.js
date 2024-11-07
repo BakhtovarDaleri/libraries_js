b5b6a0e72290afee7e29425fca26a57c4b9419f8
Bakhtovar Daleri <bakhtovarrahmatov@mail.ru>
2024-11-07 13:11:05 +0300

    Initial commit

---
+++ choices-practice/index.js
@@ -0,0 +1,65 @@
+const element = document.querySelector("#city");
+const choices = new Choices(element, {
+  searchEnabled: false,
+  shouldSortItems: false,
+  itemsSelectText: "",
+});
+
+// let str = "Hello World new ";
+// console.log(str.length);
+// console.log(str[0]);
+// console.log(str[5]);
+
+// let newStr = str.toLocaleLowerCase();
+// console.log(newStr);
+
+// console.log(str.indexOf("l"));
+// console.log(str.lastIndexOf("l"));
+
+// console.log(str.slice(0, 5));
+
+// console.log(str.substring(6, 11));
+
+// console.log(str.replace("world", "Mars"));
+
+// console.log(str.split(" "));
+
+// console.log(str.trim());
+
+// console.log(str.startsWith("Hello"));
+
+// console.log(str.endsWith("new"));
+
+// const app = document.querySelector("#app");
+// const products = ["Шкаф", "Стул", "Стол", "Шкаф"];
+
+// products.forEach((element) => {
+//   const h3El = document.createElement("h3");
+//   h3El.textContent = element;
+//   const textEl = document.createElement("p");
+//   textEl.textContent = "text info produst";
+//   const priceEl = document.createElement("p");
+//   priceEl.textContent = 2000;
+//   app.append(h3El);
+//   app.append(textEl);
+//   app.append(priceEl);
+// });
+
+// console.log(products.indexOf("Стул"));
+// console.log(products.lastIndexOf("Стул"));
+// console.log(products.includes("Тумбочка"));
+
+// console.log(products.find(item => item == 'Тумбочка'))
+
+// const array = [1000, 200, 3000, 500];
+
+// console.log(array.sort((a, b) => a - b));
+// console.log(array.sort((a, b) => b - a));
+
+// const worlds = ["h", "e", "l", "l", "o"];
+
+// console.log(worlds.join("").toUpperCase());
+
+// const productPrice = [1000, 500, 700, 2000];
+
+// console.log(productPrice.filter((item) => item > 500));
---
+++ swiper-practice/main.js
@@ -0,0 +1,12 @@
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+
+  slidePerView: 3,
+  spaceBetween: 10,
+
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
